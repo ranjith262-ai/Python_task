c26dfcc10858d7d6a2476c623a7c1085e7f2228e
ranjith262-ai <ranjith262@gmail.com>
2025-09-13 01:21:38 -0400

    first commit

---
+++ sample.py
@@ -0,0 +1,20 @@
+'''
+number = int(input("ENTER THE NUMBER: "))
+if number % 5  == 0:
+    print("number is divisible by 5")
+else:
+    print("number is not divisible by 5")
+
+number = int(input("ENTER THE NUMBER: "))
+if number % 2  == 0:
+    print("number is even")
+else:
+    print("number is odd")
+
+a = 40
+b = 20
+if a > b:
+    print("a is greater than b" ,a)
+else:
+    print("b is greater than a" ,b)
+'''
