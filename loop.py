6c7bd5a343e3bdfb1f1149db9d01dfc0edf8de85
ranjith262-ai <ranjith262@gmail.com>
2025-09-13 02:14:08 -0400

    Added loop script

---
+++ loop.py
@@ -0,0 +1,25 @@
+"""
+for num in range(1,11):
+    print(num)
+
+for char in "python":
+    print(char)
+
+add = 0
+for num in range(1,11):
+    add = num+add
+    print("Sum of:", add)
+
+department = ["CSE","IT","ECE","EEE","MECH"]
+for depar in department:
+    print("Department list:", depar)
+
+for num in range(10,20):
+    if num % 2 == 0:
+        print("Display even numbers:", num)
+        
+for num in range(20,51):
+    multiple = num * 5
+    print("Multiple of 5s:", multiple)
+"""
+    
