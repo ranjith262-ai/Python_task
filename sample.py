e84285ba8cc50c578235c6147a4a9793a85ea4dc
ranjith262-ai <ranjith262@gmail.com>
2025-09-19 22:02:29 +0530

    samplem added on windows

--- sample.py
+++ sample.py
@@ -10,11 +10,11 @@ if number % 2  == 0:
     print("number is even")
 else:
     print("number is odd")
-
+'''
 a = 40
 b = 20
 if a > b:
     print("a is greater than b" ,a)
 else:
     print("b is greater than a" ,b)
-'''
+
