faf88a50eb6e88e78358e95fdd9adc63a1451361
ranjith262-ai <ranjith262@gmail.com>
2025-09-25 22:30:30 +0530

    Added operator

---
+++ operator.py
@@ -0,0 +1,18 @@
+"""
+num1 = int(input())
+num2 = int(input())
+#num3 = 0
+if num1 > num2:
+    num3 = num1 + num2
+    print("addition of num3:" , num3)
+elif num1 < num2:
+    num3 = num1 - num2
+    print("subsraction of num3:" , num3)
+else:
+    num3 = num1 * num2
+    print("multiplication of num3:" , num3)
+"""
+
+char1 = str(input())
+char2 = str(input())
+print(char1 is char2)
