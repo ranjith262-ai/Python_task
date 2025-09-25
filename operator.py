3a776f3e775c9abfc1442a3da0860441dcd8bd0d
ranjith262-ai <ranjith262@gmail.com>
2025-09-25 22:45:10 +0530

    few lines added in operator

--- operator.py
+++ operator.py
@@ -11,8 +11,16 @@ elif num1 < num2:
 else:
     num3 = num1 * num2
     print("multiplication of num3:" , num3)
+
+char1 = str(input())
+char2 = str(input())
+char3 = char1
+#print(char1 is char2)
+#print(char1 == char2)
+print(char1 is not char2)
 """
 
 char1 = str(input())
 char2 = str(input())
-print(char1 is char2)
+print(char2 in char1)
+#print(char2 not in char1)
