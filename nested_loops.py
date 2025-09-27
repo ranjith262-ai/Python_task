fdd09fae62cf071d347abd87386a0106e9c61be2
ranjith262-ai <ranjith262@gmail.com>
2025-09-27 10:50:23 +0530

    fix

--- nested_loops.py
+++ nested_loops.py
@@ -1,5 +1,14 @@
+"""
 char1 = [["a","b","c"],["d","e","f"],["g","h","i"]]
 for row1 in char1:
     for col1 in row1:
         print(col1)
     print()
+"""
+char1 = ["red","green","blue"]
+char2 = ["apple","leaf","sky"]
+for c1 in char1:
+    for c2 in char2:
+        print(c1,c2)
+    print()
+        ]
