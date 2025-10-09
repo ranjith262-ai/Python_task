694099700e08258938af86f5afe47dc296eb13e0
ranjith262-ai <ranjith262@gmail.com>
2025-10-09 21:29:17 +0530

    Added string

--- list.py
+++ list.py
@@ -1,6 +1,7 @@
 lst = [10,20,11,16,17,22,47]
 for i in lst:
     if i%2==0:
+        #lst.append(i)
         print("Even number:", i)
     else:
         print("Odd number:", i)
--- string.py
+++ string.py
@@ -1,2 +1,10 @@
-char1 = "Hello, world"
+char1 = "Hello, World"
+char2 = '  Python Programming'
 print(char1[0])
+print(char1[1:5])
+print(char1[2:])
+print(char1[:2])
+print(char1[-5:-2])
+print(char1.upper())
+print(char1.lower())
+print(char2.strip())
