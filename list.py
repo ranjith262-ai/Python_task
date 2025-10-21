51b49cf2c65a20dbfec7b5c02be3df8b8ebc2043
ranjith262-ai <ranjith262@gmail.com>
2025-10-21 21:57:56 +0530

    added list exercise

--- list.py
+++ list.py
@@ -30,12 +30,24 @@ for i in lst6:
         lst7.append(i)
 print("List after removing duplicates:", lst7)
 
-#exercise:5
+#exercise:6
 lst8 = [10, 20, 30, 40, 50]
 lst9 = []
 for i in range(len(lst8) - 1, -1, -1):
     lst9.append(lst8[i])
 print("Reversed list:", lst9)
+
+#exercise:5
+lst10 = [10, 20, 30, 40, 50, 11, 7, 3, 90, 100]
+small_num = lst10[0]
+large_num = lst10[0]
+for i in lst10:
+    if i < small_num:
+        small_num = i
+    if i > large_num:
+        large_num = i
+print("Smallest number in the list:", small_num)
+print("Largest number in the list:", large_num)
 
 
         
