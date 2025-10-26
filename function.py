994be2720c14f4717738a35f4df29ad4c10535c2
ranjith262-ai <ranjith262@gmail.com>
2025-10-26 11:38:21 +0530

    Added function

---
+++ function.py
@@ -0,0 +1,14 @@
+def samplefun(a):
+    a = a + 10
+    print("This is a sample function:" ,a)
+samplefun(5)
+samplefun(15)
+samplefun(25)
+samplefun(35)
+samplefun(45)
+samplefun(55)
+
+def sumfun(a, b):
+    return a + b
+
+print("The sum is:", sumfun(20,100))
