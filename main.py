970c8683669efa07fd78fd83c5b67aac4faa76c7
ranjith262-ai <ranjith262@gmail.com>
2025-11-23 20:19:34 +0530

    added main script

---
+++ main.py
@@ -0,0 +1,6 @@
+import test
+import function
+
+#test.add(a,b)
+#test.subtract(a,b)
+function.square(side)
---
+++ test.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a + b
+
+print(add(4,6))
+
+def subtract(a,b):
+    return a - b
+
+print(subtract(10,5))
